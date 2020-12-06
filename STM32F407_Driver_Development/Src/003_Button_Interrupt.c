afb024559646ec09254d91cb2204b2872db3530e
aakash Bera <aakashapj@gmail.com>
2020-12-06 16:26:29 +0530

    Button Interrupt Code with Led Added

--- STM32F407_Driver_Development/Src/003_Button_Interrupt.c
+++ STM32F407_Driver_Development/Src/003_Button_Interrupt.c
@@ -14,10 +14,36 @@ void delay(int a)
 
 int main(void)
 {
+	GPIO_Handle_t gpioled, gpioBtn;
+
+		gpioled.pGPIOx = GPIOD;
+		gpioled.PinConfig.pinNumber = GPIO_PIN_NO_13;
+		gpioled.PinConfig.PinMode = PIN_MODE_OUT;
+		gpioled.PinConfig.PinOType = PIN_OTYPE_PP;
+		gpioled.PinConfig.PinOSpeed = PIN_OSPEED_HIGH;
+		gpioled.PinConfig.PinPuPd = PIN_PUPD_NONE;
+
+		GPIO_PeriClkControl(GPIOD, ENABLE);
+		GPIO_Init(gpioled);
+
+		gpioBtn.pGPIOx = GPIOA;
+		gpioBtn.PinConfig.pinNumber = GPIO_PIN_NO_0;
+		gpioBtn.PinConfig.PinMode = PIN_MODE_RT;
+		gpioBtn.PinConfig.PinOType = PIN_OTYPE_PP;
+		gpioBtn.PinConfig.PinOSpeed = PIN_OSPEED_HIGH;
+		gpioBtn.PinConfig.PinPuPd = PIN_PUPD_NONE;
+		GPIO_PeriClkControl(GPIOA, ENABLE);
+		GPIO_Init(gpioBtn);
+
+		GPIO_IRQInterruptConfig(IRQ_LINE_EXTI0, ENABLE);
+
+		while(1);
 
 }
 
 void EXTI0_IRQHandler(void)
 {
+	GPIO_TogglePin(GPIOD, GPIO_PIN_NO_13);
+	delay(1);
 	GPIO_IRQHandling(GPIO_PIN_NO_0);
 }
