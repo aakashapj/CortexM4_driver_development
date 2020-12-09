44f50a3aea35eb5a8b1fd14dce42a77f64a71d04
aakash Bera <aakashapj@gmail.com>
2020-12-09 23:03:58 +0530

    SPI Register Bit Position Macros Added

--- STM32F407_Driver_Development/driver/Inc/stm32f407xx_spi_driver.h
+++ STM32F407_Driver_Development/driver/Inc/stm32f407xx_spi_driver.h
@@ -57,6 +57,42 @@ typedef struct
 #define SPI_CPHA_FIRST				0
 #define SPI_CPHA_SECOND				1
 
+/************SPI Control Register 1 Bit Position Macros**********/
+#define SPI_CR1_CPHA				0
+#define SPI_CR1_CPOL    			1
+#define SPI_CR1_MSTR   				2
+#define SPI_CR1_BR  				3
+#define SPI_CR1_SPE 				6
+#define SPI_CR1_LSBFIRST  			7
+#define SPI_CR1_SSI       			8
+#define SPI_CR1_SSM       			9
+#define SPI_CR1_RXONLY    			10
+#define SPI_CR1_DFF       			11
+#define SPI_CR1_CRCNEXT   			12
+#define SPI_CR1_CRCEN     			13
+#define SPI_CR1_BIDIOE    			14
+#define SPI_CR1_BIDIMODE  			15
+
+/************SPI Control Register 2 Bit Position Macros**********/
+#define SPI_CR2_RXDMAEN				0
+#define SPI_CR2_TXDMAEN				1
+#define SPI_CR2_SSOE				2
+#define SPI_CR2_FRF					4
+#define SPI_CR2_ERRIE				5
+#define SPI_CR2_RXNEIE				6
+#define SPI_CR2_TXEIE				7
+
+/************SPI Status Register Bit Position Macros**********/
+#define SPI_SR_RXNE					0
+#define SPI_SR_TXE					1
+#define SPI_SR_CHSIDE				2
+#define SPI_SR_UDR					3
+#define SPI_SR_CRCERR				4
+#define SPI_SR_MODF					5
+#define SPI_SR_OVR					6
+#define SPI_SR_BSY					7
+#define SPI_SR_FRE					8
+
 /************SPI Driver Function Prototype*******************/
 void SPI_PeriClkControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 void SPI_Init(SPI_Handle_t *pSPIHandle);
