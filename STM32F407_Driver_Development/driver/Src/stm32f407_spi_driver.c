7b885f282fe2cf686d2bcbdc480210af8dee1103
aakash Bera <aakashapj@gmail.com>
2020-12-10 23:30:59 +0530

    SPI Intialization, Send Data, Recieve Data, Peripheral Control, Flag Status and Slave Select(SSOE) Programming Completed

--- STM32F407_Driver_Development/driver/Src/stm32f407_spi_driver.c
+++ STM32F407_Driver_Development/driver/Src/stm32f407_spi_driver.c
@@ -8,6 +8,45 @@
 #include "stm32f407xx.h"
 
 /*
+ * SPI Flag Status Function
+ */
+uint8_t SPI_FlagStatus(SPI_RegDef_t *pSPIx, uint8_t Flagname)
+{
+	if(pSPIx->SR & (1 << Flagname))
+	{
+		return SET;
+	}
+	return RESET;
+}
+
+/*
+ * SPI Peripheral Enable
+ */
+void SPI_PeriControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		pSPIx->CR1 |= (1 << SPI_CR1_SPE);
+	}else
+	{
+		pSPIx->CR1 &= ~(1 << SPI_CR1_SPE);
+	}
+}
+
+/*
+ * Software Slave Output Control Function
+ */
+void SPI_SSOEControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		pSPIx->CR2 |= (1 << SPI_CR2_SSOE);
+	}else
+	{
+		pSPIx->CR2 &= ~(1 << SPI_CR2_SSOE);
+	}
+}
+/*
  * SPI Peripheral Clock Enable Function
  */
 void SPI_PeriClkControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
@@ -83,7 +122,7 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 	tempreg |= (pSPIHandle->SPIConfig.SPICPHA << SPI_CR1_CPHA);
 
 	//Putting Tempreg Value into the Control Register
-	pSPIHandle->SPIx.CR1 |= tempreg;
+	pSPIHandle->pSPIx->CR1 |= tempreg;
 
 }
 
@@ -94,10 +133,43 @@ void SPI_DeInit(SPI_RegDef_t *pSPIx)
 
 void SPI_SendData(SPI_Handle_t *pSPIHandle, uint8_t *pTxBuffer, uint32_t Len)
 {
+	while(Len > 0)
+	{
+		while(!SPI_FlagStatus(pSPIHandle->pSPIx, SPI_SR_TXE));
+		if(pSPIHandle->SPIConfig.SPIDFF == SPI_DFF_8BIT)
+		{
+			pSPIHandle->pSPIx->DR = *pTxBuffer;
+			pTxBuffer++;
+			Len--;
 
+		}else if(pSPIHandle->SPIConfig.SPIDFF == SPI_DFF_16BIT)
+		{
+			pSPIHandle->pSPIx->DR = *((uint16_t*)pTxBuffer);
+			pTxBuffer++;
+			pTxBuffer++;
+			Len--;
+			Len--;
+		}
+	}
 }
 
 void SPI_ReceiveData(SPI_Handle_t *pSPIHandle, uint8_t *pRxBuffer, uint32_t Len)
 {
-
+	while(Len > 0)
+	{
+		while(!SPI_FlagStatus(pSPIHandle->pSPIx, SPI_SR_RXNE));
+		if(pSPIHandle->SPIConfig.SPIDFF == SPI_DFF_8BIT)
+		{
+			*pRxBuffer = pSPIHandle->pSPIx->DR;
+			 pRxBuffer++;
+			 Len--;
+		}else if(pSPIHandle->SPIConfig.SPIDFF == SPI_DFF_16BIT)
+		{
+			*((uint16_t*)pRxBuffer) = pSPIHandle->pSPIx->DR;
+			 pRxBuffer++;
+			 pRxBuffer++;
+			 Len--;
+			 Len--;
+		}
+	}
 }
